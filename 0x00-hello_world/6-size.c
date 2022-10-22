ea361cf5466b7cc0893e52b8e18934812a86e1fe
Olusesi Saheed Gbolahan <olusesimrgees@gmail.com>
2022-10-22 00:38:35 -0700

    modified size of computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -is the entry point for the program.
+ *
+ * Return:0 if no error, non 0 value if errors.
+ */
+int main(void)
+{
+	printf("size of a char:%id byte(s)\n", sizeof (char));
+	return (0);
+}
