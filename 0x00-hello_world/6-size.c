60ce89697fa7634312b433a4586ed011955bc81d
Olusesi Saheed Gbolahan <olusesimrgees@gmail.com>
2022-10-22 01:00:20 -0700

    modified commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * Return:0 if no error, non 0 value if errors.
  */
 int main(void)
-{i
+{
 	printf("size of a char:%id byte(s)\n", sizeof(char));
 	printf("Size of an int:%id byte(s)\n", sizeof(int));
 	printf("Size of a long int:%id byte(s)\n", sizeof(long int));
