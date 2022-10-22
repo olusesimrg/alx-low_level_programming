505447458a79feb190add20d880bf992717392f6
Olusesi Saheed Gbolahan <olusesimrgees@gmail.com>
2022-10-22 00:54:29 -0700

    modified task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
- * main -is the entry point for the program.
+ *main -is the entry point for the program.
  *
- * Return:0 if no error, non 0 value if errors.
+ *Return:0 if no error, non 0 value if errors.
  */
 int main(void)
-{
-	printf("size of a char:%id byte(s)\n", sizeof (char));
+{i
+	printf("size of a char:%id byte(s)\n", sizeof(char));
+	printf("Size of an int:%id byte(s)\n", sizeof(int));
+	printf("Size of a long int: %id byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %id byte(s)\n", sizeof(long long int));
+	printf("size of a float: %id byte(s)\n", sizeof(float));
 	return (0);
 }
