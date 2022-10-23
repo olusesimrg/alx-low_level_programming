daca05f453504f8ca5beabb6f47c466f809864c7
Olusesi Saheed Gbolahan <olusesimrgees@gmail.com>
2022-10-23 02:57:21 -0700

    modified

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
