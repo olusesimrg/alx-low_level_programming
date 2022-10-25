6720040c01660dadf560df8ee2c80be8d140a89b
Olusesi Saheed Gbolahan <olusesimrgees@gmail.com>
2022-10-24 22:01:09 -0700

    task 2 modified

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,7 +7,8 @@
 int _strlen(char *s)
 {
 	int p = 0;
-	/incremeant up to when the last character is NULL,\0/
+	/** incremeant up to when the last character is NULL,\0/ */
+
 	while (*(s + p) != 0)
 	{
 		p++;
