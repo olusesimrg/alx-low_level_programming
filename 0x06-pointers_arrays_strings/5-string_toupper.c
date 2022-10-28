ac42f88106544eb70751faa310f355c12867d719
Olusesi Saheed Gbolahan <olusesimrgees@gmail.com>
2022-10-28 12:51:42 -0700

    modified

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
  * string_toupper - changes all lowercase letters of a string
  * to uppercase
  * @s: input string.
- * 
+ *
  * Return: the pointer to dest.
  */
 char *string_toupper(char *s)
@@ -13,7 +13,9 @@ char *string_toupper(char *s)
 	while (*(s + count) != '\0')
 	{
 		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+		{
 			*(s + count) = *(s + count) - 32;
+		}
 		count++;
 	}
 	return (s);
